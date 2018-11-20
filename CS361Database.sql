c6cf4407a0a8e0bfd83b6abf85c7c3431d8442e6
wallerStephen <wallers@oregonstate.edu>
2018-11-20 07:25:44 -0700

    Fixed sql
    Removed address from create sql file arranged the insert

--- CS361Database.sql
+++ CS361Database.sql
@@ -18,14 +18,13 @@ CREATE TABLE UserAccount(
 
 CREATE TABLE Post(
 	Id INT NOT NULL AUTO_INCREMENT, 
+    UserId INT NOT NULL,
     title VARCHAR(55) NOT NULL, 
     dateOfPost DATE NOT NULL, 
     dateRequesting DATE NOT NULL, 
 	timeRequesting TIME NOT NULL, 
     message VARCHAR(1000) NOT NULL, 
-    userId INT NOT NULL, 
     pets INT NOT NULL,
-    address INT NOT NULL, 
     offerType VARCHAR(15) NOT NULL, 
     space INT NOT NULL, 
     city VARCHAR(39) NOT NULL, 
@@ -38,22 +37,22 @@ CREATE TABLE Post(
 
  
 
-
+/*test variables */
 INSERT INTO UserAccount(passwords, fname, lname, picture, age, phone, email) VALUES ("pass", "stephen", "waller", "test.com", 50, "555-555-5555", "y@gmail.com");
-INSERT INTO Post (title, dateOfPost, dateRequesting, timeRequesting, message, UserId, pets, address, offerType, space, city, street, state, zip) VALUES (
-	"NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT",  1, 2,  1, "ride", 4, "Boise", "7590  ave", "id", 8814);
+INSERT INTO Post (UserId, title, dateOfPost, dateRequesting, timeRequesting, message, pets, offerType, space, city, street, state, zip) VALUES (
+	1, "NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT", 2,  "ride", 4, "Boise", "7590  ave", "id", 8814);
     
 INSERT INTO UserAccount(passwords, fname, lname, picture, age, phone, email) VALUES ("pass", "Luke", "blanchlu", "test2.com", 50, "455-555-5555", "z@gmail.com");
-INSERT INTO Post (title, dateOfPost, dateRequesting, timeRequesting, message, UserId, pets, address, offerType, space, city, street, state, zip) VALUES (
-	"NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT",  2, 0,  1, "ride", 4, "Boise", "70  ave", "id", 8814);
+INSERT INTO Post (UserId, title, dateOfPost, dateRequesting, timeRequesting, message, pets, offerType, space, city, street, state, zip) VALUES (
+	2, "NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT", 0,  "ride", 4, "Boise", "70  ave", "id", 8814);
     
 INSERT INTO UserAccount(passwords, fname, lname, picture, age, phone, email) VALUES ("pass", "Josh", "Kinzel", "test3.com", 50, "355-555-5555", "x@gmail.com");
-INSERT INTO Post (title, dateOfPost, dateRequesting, timeRequesting, message, UserId, pets, address, offerType, space, city, street, state, zip) VALUES (
-	"NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT",  3, 9,  8, "ride", 4, "Boise", "90  ave", "id", 8814);
+INSERT INTO Post (UserId, title, dateOfPost, dateRequesting, timeRequesting, message, pets, offerType, space, city, street, state, zip) VALUES (
+	3, "NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT", 9,  "ride", 4, "Boise", "90  ave", "id", 8814);
     
 INSERT INTO UserAccount(passwords, fname, lname, picture, age, phone, email) VALUES ("pass", "Brett", "Hoelscher", "test4.com", 50, "255-555-5555", "w@gmail.com");
-INSERT INTO Post (title, dateOfPost, dateRequesting, timeRequesting, message, UserId, pets, address, offerType, space, city, street, state, zip) VALUES (
-	"NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT",  4, 5,  1, "ride", 4, "Boise", "7  ave", "id", 8814);
+INSERT INTO Post (UserId, title, dateOfPost, dateRequesting, timeRequesting, message, pets, offerType, space, city, street, state, zip) VALUES (
+	4, "NEED HELP", NOW(), NOW(), NOW(), "I AM HERE TO HELP ANYONE WHO NEEDS IT", 5,  "ride", 4, "Boise", "7  ave", "id", 8814);
 
 
 
